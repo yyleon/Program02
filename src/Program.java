d2aba16cd7346008a82d64111f3c49a04cf82e15
leon-yy <leonyy2020@gmail.com>
2021-01-10 18:07:38 +0800

    Initial commit

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,7 @@
 public class Program {
     public static void main(String[] args){
-        System.out.print("Hello world!");
+        System.out.print("Hello world!\n");
+        System.out.println("Hello sky!!\n");
+        System.out.println("Hello joking!!");
     }
 }
